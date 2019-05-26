9abc974d4a7cc650b3d718f82f30f53cfb5de92b
raksgithub <avdhesh.sharma202@gmail.com>
2019-05-26 18:26:11 +0530

    Added code for serving client side code

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./graphql/schema');
 const cors = require('cors');
+const path = require('path');
 
 // MongoDB DB connection via mongoose ORM
 require('./db/connection');
@@ -17,6 +18,12 @@ app.use('/graphql', graphqlHTTP({
 }));
 
 const port = process.env.PORT || 4000;
+
+app.use(express.static(path.join(__dirname, '/client/build')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+});
 
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
