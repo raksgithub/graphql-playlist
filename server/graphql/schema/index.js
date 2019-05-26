b50597a05410e3dff4ab97e6368b1d0a0df3872c
raksgithub <avdhesh.sharma202@gmail.com>
2019-05-26 16:27:16 +0530

    Refactor graphql types code into seperate types file in server side

--- server/graphql/schema/index.js
+++ server/graphql/schema/index.js
@@ -7,63 +7,9 @@ const {
     GraphQLInt,
     GraphQLNonNull
 } = require('graphql');
-// const { find: _find, filter: _filter } = require('lodash');
+const { BookType, AuthorType } = require('../types');
 const Book = require('../../models/book');
 const Author = require('../../models/author');
-
-const BookType = new GraphQLObjectType({
-    name: 'Book',
-    fields: () => ({
-        id: {
-            type: GraphQLID
-        },
-        name: {
-            type: GraphQLString
-        },
-        genre: {
-            type: GraphQLString
-        },
-        author: {
-            type: AuthorType,
-            async resolve(parent, args) {
-                try {
-                    const books = await Author.findById(parent.authorId);
-                    return books;
-                }
-                catch (err) {
-                    console.log('Error has occured', err);
-                }
-            }
-        }
-    })
-});
-
-const AuthorType = new GraphQLObjectType({
-    name: 'Author',
-    fields: () => ({
-        id: {
-            type: GraphQLID
-        },
-        name: {
-            type: GraphQLString
-        },
-        age: {
-            type: GraphQLInt
-        },
-        books: {
-            type: new GraphQLList(BookType),
-            async resolve(parent, args) {
-                try {
-                    const author = await Book.find({ authorId: parent.id });
-                    return author;
-                }
-                catch (err) {
-                    console.log('Error has occured', err);
-                }
-            }
-        }
-    })
-});
 
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
---
+++ server/graphql/types/index.js
@@ -0,0 +1,64 @@
+const {
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLList,
+    GraphQLID,
+    GraphQLInt,
+} = require('graphql');
+const Book = require('../../models/book');
+const Author = require('../../models/author');
+const BookType = new GraphQLObjectType({
+    name: 'Book',
+    fields: () => ({
+        id: {
+            type: GraphQLID
+        },
+        name: {
+            type: GraphQLString
+        },
+        genre: {
+            type: GraphQLString
+        },
+        author: {
+            type: AuthorType,
+            async resolve(parent, args) {
+                try {
+                    const books = await Author.findById(parent.authorId);
+                    return books;
+                }
+                catch (err) {
+                    console.log('Error has occured', err);
+                }
+            }
+        }
+    })
+});
+
+const AuthorType = new GraphQLObjectType({
+    name: 'Author',
+    fields: () => ({
+        id: {
+            type: GraphQLID
+        },
+        name: {
+            type: GraphQLString
+        },
+        age: {
+            type: GraphQLInt
+        },
+        books: {
+            type: new GraphQLList(BookType),
+            async resolve(parent, args) {
+                try {
+                    const author = await Book.find({ authorId: parent.id });
+                    return author;
+                }
+                catch (err) {
+                    console.log('Error has occured', err);
+                }
+            }
+        }
+    })
+});
+
+module.exports = { AuthorType, BookType };
